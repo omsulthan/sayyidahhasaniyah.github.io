b6194f075da91e94aaa3084bca2561e140d2ae31
Sulthan <mromsulthan@gmail.com>
2024-03-12 10:35:10 +0700

    add landing and program page

---
+++ index.php
@@ -0,0 +1,193 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="src/css/style.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" rel="stylesheet">
+</head>
+
+<body>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg bg-white sticky-top">
+    <div class="container-fluid container-lg">
+      <a class="navbar-brand" href="index.php">
+        <img src="src/img/logo.png" class="object-cover" width="50" alt="...">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav text-center">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Beranda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="program.php">Program</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="tentang.php">Tentang</a>
+          </li>
+          <li class="nav-item text-center d-inline-block d-lg-none">
+            <a class="nav-link" href="tentang.php">Gabung</a>
+          </li>
+        </ul>
+      </div>
+      <button class="btn bg-primary-color d-none d-lg-block">Gabung</button>
+    </div>
+  </nav>
+  <!-- End OF Navbar -->
+
+  <div style="background-image: url('src/img/hero-bg.png');background-position: center;background-repeat: no-repeat;background-size: cover; padding: 160px 0px" class=" h-100 text-center d-flex flex-column justify-content-center align-items-center gap-4">
+    <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-center" class="fw-bold col-10 col-lg-6 hero-title">Yayasan <span class="primary-color">Sayyidul Hasaniyah</span></div>
+    <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-center" class="fw-normal col-10 col-lg-6 hero-subtitle">
+      Berperan menciptakan masa depan cerah bagi anak yatim melalui akses pendidikan, kesehatan, dan perlindungan.</div>
+    <a href="#pe" data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-center" class="btn bg-primary-color mt-3 link-decoration-none">Pelajari lebih lanjut</a>
+  </div>
+
+  <div id="pe" data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-center" class="py-5 container d-flex flex-column flex-lg-row gap-5 align-items-center">
+    <div style=" background-image: url('src/img/11.jpg');" class="col-10 col-lg-6 rounded image-goal"></div>
+    <div class="col-10 col-lg-6 d-flex flex-column gap-3 text-center text-lg-start">
+      <div class="fw-bold font-goal">Perlindungan</div>
+      <div class="fw-medium font-goal-subtitle">Lembaga kami bertujuan memberikan perlindungan, dan pendidikan, menciptakan lingkungan aman dan mendukung bagi pertumbuhan mereka. Dengan akses kesehatan dan program pembinaan, kami membantu mereka berkembang menjadi individu mandiri dan berkontribusi positif dalam masyarakat.</div>
+    </div>
+  </div>
+
+  <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-center" class="py-5 container d-flex flex-column-reverse flex-lg-row gap-5 align-items-center mt-3">
+    <div class="col-10 col-lg-6 d-flex flex-column gap-3 text-center text-lg-start">
+      <div class="fw-bold font-goal">Kesempatan</div>
+      <div class="fw-medium font-goal-subtitle">Kami berkomitmen untuk menciptakan kesempatan yang adil bagi anak-anak yatim dalam mengakses pendidikan, kesehatan, dan peluang lainnya agar mereka dapat meraih cita-cita mereka dengan penuh potensi dan harapan.</div>
+      <div>
+        <a href="program.php" class="btn bg-primary-color col-4 col-lg btn-sm link-decoration-none">Lanjut</a>
+      </div>
+    </div>
+    <div style=" background-image: url('src/img/10.jpg');" class="col-10 col-lg-6 rounded image-goal"></div>
+  </div>
+
+  <div class="bg-white py-5">
+    <div class="container">
+      <div class="fs-1 fw-semibold text-center">Misi</div>
+      <div class="fs-6 fw-light text-center">Kami Berkomitmen Menciptakan</div>
+      <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 gy-3 g-lg-5 mt-0">
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-center" class="d-flex flex-column gap-2">
+          <div class="fs-3 fw-medium">Pendidikan Berkualitas</div>
+          <div style="text-align: justify;" class="fs-6">Memberikan layanan pendidikan berkualitas bagi anak-anak yatim agar memiliki keterampilan dan pengetahuan yang memadai untuk mencapai cita-cita mereka.
+          </div>
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-center" class="d-flex flex-column gap-2">
+          <div class="fs-3 fw-medium">Lingkungan Sejahtera</div>
+          <div style="text-align: justify;" class="fs-6">Menyediakan lingkungan yang aman dan nyaman bagi anak-anak yatim untuk tumbuh dan berkembang secara fisik, emosional, dan sosial
+          </div>
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-center" class="d-flex flex-column gap-2">
+          <div class="fs-3 fw-medium">Kesehatan Terjamin</div>
+          <div style="text-align: justify;" class="fs-6"> Memberikan bantuan kesehatan dan akses terhadap layanan medis yang diperlukan untuk menjaga kesehatan dan kesejahteraan anak-anak yatim.
+          </div>
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-center" class="d-flex flex-column gap-2">
+          <div class="fs-3 fw-medium">Fasilitas Memadai</div>
+          <div style="text-align: justify;" class="fs-6">Memfasilitasi pembinaan karakter dan nilai-nilai moral serta agama bagi anak-anak yatim untuk membentuk kepribadian yang baik dan bertanggung jawab.
+          </div>
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-center" class="d-flex flex-column gap-2">
+          <div class="fs-3 fw-medium">Hubungan Baik</div>
+          <div style="text-align: justify;" class="fs-6">Berkolaborasi dengan berbagai pihak, termasuk pemerintah, lembaga swadaya masyarakat, dan masyarakat umum, untuk meningkatkan dukungan dan akses bagi anak-anak yatim.
+          </div>
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-center" class="d-flex flex-column gap-2">
+          <div class="fs-3 fw-medium">Penggalangan Dana</div>
+          <div style="text-align: justify;" class="fs-6">Menggalang sumber daya dan dukungan finansial untuk mendukung program-program yayasan dan meningkatkan dampak positifnya terhadap kehidupan anak-anak yatim.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container py-5">
+    <div class="fs-1 fw-semibold text-center mb-3">Galeri</div>
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 gy-3 g-lg-5">
+      <div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-center" style=" background-image: url('src/img/12.jpg');background-position: center;background-repeat: no-repeat;background-size: cover; height : 30vh" class="rounded"></div>
+      </div>
+      <div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-center" style=" background-image: url('src/img/2.jpg');background-position: center;background-repeat: no-repeat;background-size: cover; height : 30vh" class="rounded"></div>
+      </div>
+      <div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-center" style=" background-image: url('src/img/3.jpg');background-position: center;background-repeat: no-repeat;background-size: cover; height : 30vh" class="rounded"></div>
+      </div>
+      <div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-center" style=" background-image: url('src/img/4.jpg');background-position: center;background-repeat: no-repeat;background-size: cover; height : 30vh" class="rounded"></div>
+      </div>
+      <div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-center" style=" background-image: url('src/img/14.jpg');background-position: center;background-repeat: no-repeat;background-size: cover; height : 30vh" class="rounded"></div>
+      </div>
+      <div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-center" style=" background-image: url('src/img/6.jpg');background-position: center;background-repeat: no-repeat;background-size: cover; height : 30vh" class="rounded"></div>
+      </div>
+      <div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-center" style=" background-image: url('src/img/7.jpg');background-position: center;background-repeat: no-repeat;background-size: cover; height : 30vh" class="rounded"></div>
+      </div>
+      <div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-center" style=" background-image: url('src/img/8.jpg');background-position: center;background-repeat: no-repeat;background-size: cover; height : 30vh" class="rounded"></div>
+      </div>
+      <div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-center" style=" background-image: url('src/img/9.jpg');background-position: center;background-repeat: no-repeat;background-size: cover; height : 30vh" class="rounded"></div>
+      </div>
+    </div>
+  </div>
+
+  <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-center" class="container py-5">
+    <div class="fs-6 fw-light text-center mb-3 col-10 mx-auto mb-4">"Seorang aktivis sosial yang memiliki kepedulian mendalam terhadap anak-anak yatim di Indonesia, berdedikasi untuk meningkatkan kesejahteraan mereka melalui berbagai inisiatif dan program yang bertujuan memberikan akses yang adil terhadap pendidikan, kesehatan, dan peluang lainnya."</div>
+    <div class="d-flex justify-content-center"><img src="src/img/profile.jpg" alt="" class="rounded-circle" width="80" height="80"></div>
+    <div class="d-flex justify-content-center mt-3 fs-4 fw-medium">Hasan Sumarji</div>
+    <div class="d-flex justify-content-center fs-5 fw-normal">Pendiri Yayasan Sayyidul Hasaniyah</div>
+  </div>
+
+  <!-- Footer -->
+  <div class="bg-white">
+    <div class="py-5 container d-flex flex-column flex-lg-row gap-5">
+      <div data-aos="fade-right" data-aos-duration="1000" class="col-12 col-lg-6 d-flex flex-column gap-2">
+        <div class="fw-bold fs-1">Hubungi Kami</div>
+        <div class="fs-6 fw-normal"><a target="_blank" class="link-underline primary-color link-underline-opacity-0 d-flex align-items-center gap-1" href="https://api.whatsapp.com/send?phone=6281212341305&text=Assalamualaikum%20Pak%20Hasan"><i class="fa-brands fa-whatsapp fs-3"></i>Whatsapp</a></div>
+        <div class="fs-6 fw-normal">yayasansayyinulhasaniyah@gmail.com</div>
+        <div class="fs-6 fw-normal col-10">Jl. Bintara VIII No.1b, RT.004/RW.003, Bintara, Kec. Bekasi Bar., Kota Bks, Jawa Barat 17134</div>
+        <div class="d-flex gap-3 fw-normal fs-3 align-items-center">
+          <div><a target="_blank" class="link-underline text-black link-underline-opacity-0 d-flex align-items-center gap-1" href="https://www.instagram.com/yayasan_sayyidul_hasaniyah/"><i class="fa-brands fa-instagram"></i></a></div>
+          <div><a target="_blank" class="link-underline text-black link-underline-opacity-0 d-flex align-items-center gap-1" href="https://youtube.com/@hasansumarji4686?si=lovHMNKlKu0QGscp"><i class="fa-brands fa-youtube"></i></a></div>
+          <div><a target="_blank" class="link-underline text-black link-underline-opacity-0 d-flex align-items-center gap-1" href="https://www.snackvideo.com/@HSN1972"><img src="src/img/snack-video.svg" width="32" alt=""></a></div>
+        </div>
+      </div>
+      <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-bottom" class="col-12 col-lg-6 d-flex flex-column gap-3">
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Nama Pengirim</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Email</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3">
+            <label for="floatingTextarea" class="form-label">Pesan</label>
+            <textarea class="form-control" placeholder="Masukan Pesan Disini" id="floatingTextarea"></textarea>
+          </div>
+          <button type="submit" class="btn bg-primary-color col px-5">Kirim</button>
+        </form>
+      </div>
+    </div>
+  </div>
+
+</body>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/bb1fce9dd9.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
+
+</html>
---
+++ program.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="src/css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-white sticky-top">
+        <div class="container-fluid container-lg">
+            <a class="navbar-brand" href="index.php">
+                <img src="src/img/logo.png" class="object-cover" width="50" alt="...">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav text-center">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="program.php">Program</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="tentang.php">Tentang</a>
+                    </li>
+                    <li class="nav-item text-center d-inline-block d-lg-none">
+                        <a class="nav-link" href="tentang.php">Gabung</a>
+                    </li>
+                </ul>
+            </div>
+            <button class="btn bg-primary-color d-none d-lg-block">Gabung</button>
+        </div>
+    </nav>
+    <!-- End OF Navbar -->
+
+    <div style="background-image: url('src/img/hero-bg.png');background-position: center;background-repeat: no-repeat;background-size: cover; padding: 160px 0px" class=" h-100 text-center d-flex flex-column justify-content-center align-items-center gap-4">
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-center" class="fw-bold col-10 col-lg-6 hero-title">Yayasan <span class="primary-color">Sayyidul Hasaniyah</span></div>
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-center" class="fw-normal col-10 col-lg-6 hero-subtitle">
+            Berperan menciptakan masa depan cerah bagi anak yatim melalui akses pendidikan, kesehatan, dan perlindungan.</div>
+        <a href="#pe" data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-center" class="btn bg-primary-color mt-3 link-decoration-none">Pelajari lebih lanjut</a>
+    </div>
+
+    <div id="pe" class="container py-5">
+        <div class="text-center fs-1 fw-bold">Program Unggulan</div>
+        <div class="row row-cols-1 row-cols-md-2 gy-5 g-lg-5 mt-0 text-center justify-content-center">
+            <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-bottom" class="d-flex flex-column justify-content-center align-items-center">
+                <div><img src="src/img/01.png" class="rounded-circle object-cover" width="200" height="200" alt="Exhibition Image"></div>
+                <div class="fs-2 fw-semibold">Program Pendidikan</div>
+                <div class="col-9 ">Menyediakan beasiswa pendidikan bagi anak-anak yatim dari tingkat Sekolah Dasar (SD) hingga Sekolah Menengah Atas (SMA)</div>
+            </div>
+            <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-bottom" class="d-flex flex-column justify-content-center align-items-center">
+                <div><img src="src/img/02.jpg" class="rounded-circle object-cover" width="200" height="200" alt="Exhibition Image"></div>
+                <div class="fs-2 fw-semibold">Program Kesehatan</div>
+                <div class="col-9 ">Program ini juga mencakup memberikan layanan kesehatan gratis dan penyuluhan kesehatan kepada anak-anak yatim dan keluarganya.</div>
+            </div>
+            <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-bottom" class="d-flex flex-column justify-content-center align-items-center">
+                <div><img src="src/img/03.png" class="rounded-circle object-cover" width="200" height="200" alt="Exhibition Image"></div>
+                <div class="fs-2 fw-semibold">Program Pembinaan</div>
+                <div class="col-9 ">Mengadakan kegiatan pembinaan karakter, keterampilan, dan kewirausahaan bagi anak-anak yatim.</div>
+            </div>
+            <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-bottom" class="d-flex flex-column justify-content-center align-items-center">
+                <div><img src="src/img/04.png" class="rounded-circle object-cover" width="200" height="200" alt="Exhibition Image"></div>
+                <div class="fs-2 fw-semibold">Program Kemitraan</div>
+                <div class="col-9 ">Berkolaborasi dengan berbagai lembaga dan perusahaan untuk memberikan pelatihan kerja dan kesempatan kerja bagi anak-anak yatim yang telah dewasa.</div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <div class="bg-white">
+        <div class="py-5 container d-flex flex-column flex-lg-row gap-5">
+            <div data-aos="fade-right" data-aos-duration="1000" class="col-12 col-lg-6 d-flex flex-column gap-2">
+                <div class="fw-bold fs-1">Hubungi Kami</div>
+                <div class="fs-6 fw-normal"><a target="_blank" class="link-underline primary-color link-underline-opacity-0 d-flex align-items-center gap-1" href="https://api.whatsapp.com/send?phone=6281212341305&text=Assalamualaikum%20Pak%20Hasan"><i class="fa-brands fa-whatsapp fs-3"></i>Whatsapp</a></div>
+                <div class="fs-6 fw-normal">yayasansayyinulhasaniyah@gmail.com</div>
+                <div class="fs-6 fw-normal col-10">Jl. Bintara VIII No.1b, RT.004/RW.003, Bintara, Kec. Bekasi Bar., Kota Bks, Jawa Barat 17134</div>
+                <div class="d-flex gap-3 fw-normal fs-3 align-items-center">
+                    <div><a target="_blank" class="link-underline text-black link-underline-opacity-0 d-flex align-items-center gap-1" href="https://www.instagram.com/yayasan_sayyidul_hasaniyah/"><i class="fa-brands fa-instagram"></i></a></div>
+                    <div><a target="_blank" class="link-underline text-black link-underline-opacity-0 d-flex align-items-center gap-1" href="https://youtube.com/@hasansumarji4686?si=lovHMNKlKu0QGscp"><i class="fa-brands fa-youtube"></i></a></div>
+                    <div><a target="_blank" class="link-underline text-black link-underline-opacity-0 d-flex align-items-center gap-1" href="https://www.snackvideo.com/@HSN1972"><img src="src/img/snack-video.svg" width="32" alt=""></a></div>
+                </div>
+            </div>
+            <div data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-bottom" class=" col-12 col-lg-6 d-flex flex-column gap-3">
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Nama Pengirim</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Email</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3">
+                        <label for="floatingTextarea" class="form-label">Pesan</label>
+                        <textarea class="form-control" placeholder="Masukan Pesan Disini" id="floatingTextarea"></textarea>
+                    </div>
+                    <button type="submit" class="btn bg-primary-color col px-5">Kirim</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/bb1fce9dd9.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
+
+</html>
